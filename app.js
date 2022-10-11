282229ba4d31f41f591d5a7f28953302811f5237
Benkalio <officialtennysonbk@gmail.com>
2022-10-11 14:01:21 +0100

    added database for users

--- app.js
+++ app.js
@@ -1,6 +1,41 @@
 const express = require('express')
 const app = express()
-const PORT = 5000;
+const mustacheExpress = require('mustache-express');
+const bodyParser = require('body-parser');
+const pgp = require('pg-promise')()
+const PORT = 4000;
+const CONNECTION_STRING = "postgres://localhost:5432/appdb";
+
+app.engine('mustache', mustacheExpress())
+app.set('views', './views')
+app.set('view engine', 'mustache');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//  CONNECT TO THE POSTGRES DATABASE 
+const db = pgp(CONNECTION_STRING);
+
+app.post('/register', (req, res) => {
+  let username = req.body.username;
+  let password = req.body.password;
+
+  db.oneOrNone('SELECT userid FROM users WHERE username = $1',[username])
+    .then((user) => {
+      if (user) {
+        res.render('register', {message: "Username already exists!"})
+      } else {
+        //  INSERT USER INTO THE USERS table
+        db.none('INSERT INTO users(username,password) VALUES($1,$2)',[username, password])
+          .then(() => {
+            res.send('Succeeded')
+          })
+      }
+    })
+})
+
+app.get('/register', (req, res) => {
+  res.render('register');
+});
 
 app.listen(PORT, () => {
   console.log(`Server has started on ${PORT}`);
